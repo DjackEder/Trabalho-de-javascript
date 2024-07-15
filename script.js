09c06305b33838d79d070593a70449da3e680131
Eder pires <ederpires1032gmail.com>
2024-07-15 20:11:46 -0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const primeiro = document.getElementById("primeiro")
+const segundo = document.getElementById("segundo")
+let mudar = 0;
+
+primeiro.addEventListener("click", function passarSlide() {
+    
+})
+segundo.addEventListener("click", function passarSlide() {
+    
+})
+
+const imgGrande = document.getElementById("imgGrande")
+imgGrande.addEventListener("click", function mudarTamanho(params) {
+    
+})
